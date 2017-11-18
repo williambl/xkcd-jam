823b019156da4291609e906966e3d2a4fc9fa896
williambl <william@williambl.com>
2017-11-18 12:09:53 +0000

    Trying to get movement to work

---
+++ xkcd_jam/Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
+public class Move : MonoBehaviour {
+
+    RaycastHit hitInfo = new RaycastHit();
+    UnityEngine.AI.NavMeshAgent agent;
+
+    void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+    }
+
+    void Update () {
+        if(Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+                agent.destination = hitInfo.point;
+        }
+    }
+}
