b21668712459d5d133ef1286ab7a84aa47e686ac
williambl <william@williambl.com>
2017-11-18 22:16:31 +0000

    Added health to UI

--- xkcd_jam/Assets/Scripts/PlayerHealth.cs
+++ xkcd_jam/Assets/Scripts/PlayerHealth.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour {
 
         public float health = 100;
+        public UnityEngine.UI.Text text;
 
         public void LoseHealth (float amount) {
             health -= amount;
+        }
+
+        void Update () {
+            text.text = health.ToString();
         }
 }
