4981f17dc4e07bece07edc527da66611f25db154
williambl <william@williambl.com>
2017-11-18 21:47:34 +0000

    NPCs now shoot

--- xkcd_jam/Assets/Scripts/MoveNPC.cs
+++ xkcd_jam/Assets/Scripts/MoveNPC.cs
@@ -20,9 +20,20 @@ public class MoveNPC : MonoBehaviour {
 
     void Update () {
         if (Vector3.Distance(transform.position, player.position) < 10) {
-            agent.destination = player.position;
+            if (!isChasingPlayer)
+                StartCoroutine(Shoot ());
+
+            if (Vector3.Distance(transform.position, player.position) < 3)
+                agent.destination = transform.position;
+            else
+                agent.destination = player.position;
             isChasingPlayer = true;
+        } else {
+            if (isChasingPlayer)
+                StopCoroutine(Shoot ());
+            isChasingPlayer = false;
         }
+
         if (!isChasingPlayer && agent.remainingDistance < 0.1)
             Patrol();
 
@@ -34,5 +45,19 @@ public class MoveNPC : MonoBehaviour {
 
     void Patrol () {
         agent.destination = points[Random.Range(0, points.Length-1)].position;
+    }
+
+    IEnumerator Shoot () {
+        while (true) {
+            RaycastHit hit;
+            Vector3 position = transform.position + new Vector3(0,1,0);
+            if (Physics.Raycast(position, transform.forward, out hit)) {
+                Debug.DrawLine(position, hit.point, Color.cyan, 2f);
+                if(hit.collider.gameObject.tag == "Player") {
+                }
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
