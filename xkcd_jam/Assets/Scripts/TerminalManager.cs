fec22dbe30837bd0051bcf7a7d0984435dba6a48
williambl <william@williambl.com>
2017-11-19 10:41:05 +0000

    You now win if you activate all terminals

---
+++ xkcd_jam/Assets/Scripts/TerminalManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalManager : MonoBehaviour {
+
+        public TerminalInteract[] terminals;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   if (CheckTerminals())
+              Debug.Log("Win!");
+	}
+
+        bool CheckTerminals () {
+            foreach (TerminalInteract terminal in terminals) {
+                if (!terminal.interacted) {
+                    Debug.Log("No");
+                    return false;
+                }
+            }
+            return true;
+        }
+}
