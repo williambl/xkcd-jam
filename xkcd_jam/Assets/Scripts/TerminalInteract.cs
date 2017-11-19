311f1fc8d340f1b86cb9ed86c5cca00eed4e0383
williambl <william@williambl.com>
2017-11-19 10:10:52 +0000

    You can now interact with terminals

---
+++ xkcd_jam/Assets/Scripts/TerminalInteract.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalInteract : MonoBehaviour {
+
+        int playerMask;
+
+        public bool interacted = false;
+
+	// Use this for initialization
+	void Start () {
+	    playerMask = LayerMask.GetMask("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("Use")) {
+                if (Physics.CheckSphere(transform.position, 2f, playerMask)) {
+                    interacted = true;
+                }
+            }
+	}
+}
